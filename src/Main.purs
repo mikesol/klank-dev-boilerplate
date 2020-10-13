eabd52373503feacc3c36e1da4c9bea27fbd0110
Mike Solomon <mike@meeshkan.com>
2020-10-13 21:17:47 +0300

    Changes to klank session

--- src/Main.purs
+++ src/Main.purs
@@ -1,29 +1,18 @@
-module Main where
+module Klank.Dev where
 
 import Prelude
-import Data.List ((:), List(..))
-import Data.NonEmpty ((:|))
 import Data.Typelevel.Num (D1)
-import Effect (Effect)
-import Effect.Class.Console (log)
 import FRP.Behavior (Behavior)
-import FRP.Behavior.Audio (AudioUnit, gain', sinOsc, speaker)
-import Math (pi, sin)
+import FRP.Behavior.Audio
+  ( AudioUnit
+  , gain'
+  , runInBrowser
+  , sinOsc
+  , squareOsc
+  , speaker'
+  )
 
 scene :: Number -> Behavior (AudioUnit D1)
-scene time =
-  let
-    rad = pi * time
-  in
-    pure
-      $ speaker
-          ( (gain' 0.1 $ sinOsc (440.0 + (10.0 * sin (2.3 * rad))))
-              :| (gain' 0.25 $ sinOsc (235.0 + (10.0 * sin (1.7 * rad))))
-              : (gain' 0.2 $ sinOsc (337.0 + (10.0 * sin rad)))
-              : (gain' 0.1 $ sinOsc (530.0 + (19.0 * (5.0 * sin rad))))
-              : Nil
-          )
+scene _ = pure (speaker' (gain' 0.2 (squareOsc 40.0)))
 
-main :: Effect Unit
-main = do
-  log "🍝"
+main = runInBrowser scene
